49c25f81456d649a2207c05fe856850247c38dd4
Maná Machado <manamachado8@gmail.com>
2020-03-22 22:01:20 -0300

    Updates

---
+++ precache-manifest.d837f8c19bb684b98d8e35fcf71a46fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30e62eed3134048c755ba28cbb3bc745",
+    "url": "/huntweb/index.html"
+  },
+  {
+    "revision": "43bfa9d858787a0709ff",
+    "url": "/huntweb/static/css/main.d582f89f.chunk.css"
+  },
+  {
+    "revision": "655109d29851ebefee45",
+    "url": "/huntweb/static/js/2.a91e671c.chunk.js"
+  },
+  {
+    "revision": "6b1c9aa1f24077e4bfd4b51db2988c14",
+    "url": "/huntweb/static/js/2.a91e671c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43bfa9d858787a0709ff",
+    "url": "/huntweb/static/js/main.2a3dbf77.chunk.js"
+  },
+  {
+    "revision": "abce4e3f8dbf97f67488",
+    "url": "/huntweb/static/js/runtime-main.26bbd4b3.js"
+  }
+]);
